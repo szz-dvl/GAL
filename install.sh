35ceeb457a1ba6f575f679a4fb6581e205d7a40a
szz-dvl <santiaguzz@gmail.com>
2018-03-21 19:48:45 +0100

    Installation script

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 pwd=`pwd`
 
-sed "s|\[PWD\]|$pwd|gi" launch.sh > ./launch.sh
+sed "s|\[PWD\]|$pwd|gi" launch.sh > launch.sh
 sed "s|\[PWD\]|$pwd|gi" services/pad-listener.service | sudo tee /lib/systemd/system/pad-listener.service
 sed "s|\[PWD\]|$pwd|gi" services/gal.service | sudo tee /lib/systemd/system/gal.service
